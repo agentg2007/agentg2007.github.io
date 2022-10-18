56e9e18ebab63219e9be5c19db1816fdb32c325d
agentg2007 <allen.gepulla@gmail.com>
2022-10-18 15:51:03 +0800

    updated compilation runtimes

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,8 @@ const common = {
     resolve: {
         extensions: [".js", ".ts", ".tsx"],
         alias: {
-            'react': path.resolve(__dirname, './node_modules/react'),
-            "styled-components": path.resolve(__dirname, "./node_modules/styled-components"),
+            // 'react': path.resolve(__dirname, './node_modules/react'),
+            // "styled-components": path.resolve(__dirname, "./node_modules/styled-components"),
             "@components": path.resolve(__dirname, "./src/components/index.ts"),
             "@mui-components": path.resolve(__dirname, "./src/components/mui-components/index.ts"),
         },
@@ -34,11 +34,15 @@ module.exports = [{
         app: {
             import: "./src/index.tsx",
             dependOn: [
-                "react", "mui"
+                "mui",
+                "react",
+                "router",
             ]
         },
         mui: ["@mui/material", "@emotion/react", "@emotion/styled"],
-        react: ["react", "react-dom"],
+        react: ["react", "react-dom",],
+        router: ["react-router-dom"],
+        //TODO: Remove "@nthity/react-menu", will cause large compile file,
     },
     output: {
         path: path.join(__dirname, "scripts"),
